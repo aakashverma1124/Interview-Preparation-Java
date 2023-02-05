11ac2d2aa3f7c134787d8b708a226c732b70432d
Aakash Verma <aakashverma@Aakashs-MacBook-Pro.local>
2023-02-05 15:32:16 +0530

    BFS Addded

--- src/graph/AdjacencyList.java
+++ src/graph/AdjacencyList.java
@@ -26,10 +26,35 @@ public class AdjacencyList {
         }
     }
 
+    public static List<Integer> breadthFirstSearch(int v, int[][] edges, int src) {
+        List<List<Integer>> graph = AdjacencyList.buildGraph(v, edges);
+
+        boolean[] visited = new boolean[v + 1];
+        Queue<Integer> queue = new LinkedList<Integer>();
+        queue.offer(src);
+        visited[src] = true;
+
+        List<Integer> bfs = new ArrayList<>();
+
+        while(!queue.isEmpty()) {
+            int node = queue.poll();
+            bfs.add(node);
+            for(int nbr : graph.get(node)) {
+                if(!visited[nbr]) {
+                    queue.offer(nbr);
+                    visited[nbr] = true;
+                }
+            }
+        }
+        return bfs;
+
+    }
+
     public static void main(String[] args) {
-        int v = 5;
-        int[][] edges = new int[][]{{1, 2}, {1, 4}, {1, 5}, {2, 3}, {2, 5}, {3, 5}};
+        int v = 6;
+        int[][] edges = new int[][]{{1, 2}, {1, 5}, {2, 3}, {2, 4}, {3, 4}, {3, 6}, {4, 6}, {5, 6}};
         AdjacencyList.printGraph(v, edges);
+        System.out.println(AdjacencyList.breadthFirstSearch(v, edges, 1));
     }
     
 }
